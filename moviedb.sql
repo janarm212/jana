9d078a874f7977f4a3a9922b957c27f9368f1b75
navalord <93308686+navalord@users.noreply.github.com>
2022-07-01 20:13:21 +0530

    Create moviedb.sql

---
+++ moviedb.sql
@@ -0,0 +1,6 @@
+create table movie(movie_name varchar(100),lead_actor_name varchar(100),actress_name varchar(100),year_of_releas INTEGER,director_name varchar(100));
+insert into movie values("KGF1","YASH","SRINITHI",2019,"PRASANTH");
+insert into movie values("KGF2","YASH","SRINITHI",2022,"PRASANTH");
+insert into movie values("BAHUBALLI1","PRABASH","TAMANA",2018,"RAJMOULI");
+insert into movie values("BAHUBALLI2","PRABASH","TAMANA",2020,"RAJMOULI");
+select*from movie;
